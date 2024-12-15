f82e3980ec00d10e43298ce93e443bd108281aa5
Olive <oliveistyping@gmail.com>
2024-12-15 08:11:03 -0700

    Deprecate ClassicIE
    cmon people its almost 2025......

--- Src/ClassicIE/ClassicIEDLL/SettingsUI.h
+++
@@ -1,8 +0,0 @@
-// Classic Shell (c) 2009-2017, Ivo Beltchev
-// Retro-Shell (c) 2024, aubymori
-// Retro-Shell (c) 2025, Olive6841
-// Confidential information of Ivo Beltchev. Not for disclosure or distribution without prior written consent from the author
-
-#pragma once
-
-void InitSettings(void);
